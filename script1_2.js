f3ca944713714210eb61c13c7b73772236ab3b86
Rabia Söylemez <ing.rabia.soeylemez@gmail.com>
2024-07-25 12:15:29 +0200

    aktueller Stand

--- script1_2.js
+++ script1_2.js
@@ -1,12 +1,11 @@
-let startUrl = "https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0";
+let startUrl = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0";
 let typeUrl = "https://pokeapi.co/api/v2/pokemon/";
 let amount = 0;
 let startAmount = 0;
 let allPokemons = [];
 let filteredPokemons = [];
 let inputField;
-let loadAboutsVa = true;
-let loadBasestateVa = false;
+let pokemon = [];
 
 document.addEventListener('DOMContentLoaded', (event) => {
     init();
@@ -19,49 +18,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
             closePokemonCard();
         }
     });
-
     // Event Listener zum Verhindern des Schließens, wenn innerhalb des pokemonInfoCard geklickt wird
     document.getElementById('pokemonInfoCard').addEventListener('click', (event) => {
         event.stopPropagation(); // Verhindert das Weiterleiten des Click-Events
     });
-
-    // Event Listener für das Suchfeld
-    document.getElementById('searchField').addEventListener('input', readInput);
-});
+})
 
 function init() {
     renderStartPage();
 }
 
-function readInput() {
-    inputField = document.getElementById('searchField').value; // 'searchField' sollte die ID des Eingabefelds sein
-    inputField = inputField.toLowerCase(); // Umwandeln in Kleinbuchstaben
-    filterPokemons(inputField); // Hier rufen wir die filterPokemons-Funktion auf
-}
-
-function filterPokemons(inputField) {
-    filteredPokemons = allPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(inputField));
-    // Alle Pokemons, die durch den Filter kommen, werden im Array gespeichert
-    amount = 0;
-    startAmount = 0;
-    document.getElementById('content').innerHTML = '';
-    console.log('filteredPokemons:'+filterPokemons);
-    renderMorePokemons({results: filteredPokemons}); // Hier die gefilterte Liste anzeigen
-}
-
-//um die Liste auf der Console einmal anzusehen
-function allPokemonsList(){
-    let result= [];
-    for(i=0; i<100; i++){
-        result += allPokemons[i]['name'];
-    }
-    return result;
-}
-
 async function renderStartPage() {
     let content = document.getElementById('content');
     let pokemons = await loadMainPokemonData();
-    filteredPokemons = allPokemons; // initially, show all pokemons
     content.innerHTML = '';
     await renderMorePokemons(pokemons);
     await loadMoreButton(pokemons);
@@ -69,20 +38,20 @@ async function renderStartPage() {
 
 async function renderMorePokemons(pokemons) {
     let content = document.getElementById('content');
-    amount = amount + Math.min(startAmount + 20, pokemons.results.length);
+    amount = amount +10;
     for (let i = startAmount; i < amount; i++) {
-        let pokemon = await loadPokemon(pokemons.results[i].name);
+        pokemon = await loadPokemon(pokemons.results[i].name);
         let name = capitalizeFirstLetter(pokemons.results[i].name); // for Capital letter
         content.innerHTML += renderStartPageHTML(name, i, pokemon);
-        colorOfCard(pokemon, i);
     }
+    startAmount = startAmount+ 10;
     addEventListeners(pokemons, startAmount, amount);
-    startAmount += amount;
+    
 }
 
 function renderStartPageHTML(name, i, pokemon) {
     return `
-    <div class="pokemonCards" id="pokemon-${i}">
+    <div class="pokemonCards ${pokemon.types[0].type.name}" id="pokemon-${i}">
         <h2 class="pokemonName">${name}</h2>
         <div class="typeAndImg">
            <div>${pokemonType(pokemon)} </div>
@@ -111,58 +80,6 @@ function addEventListeners(pokemons, start, end) {
                 openPokemoncard(i, pokemons);
             });
         }
-    }
-}
-
-function colorOfCard(pokemon, i) {
-    let content = document.getElementById(`pokemon-${i}`);
-    if (content){
-        content.classList = '';
-        content.classList.add('pokemonCards');
-
-    }
-    switch (pokemon.types[0].type.name) {
-        case "grass":
-            content.classList.add('green');
-            break;
-        case "fire":
-            content.classList.add('red');
-            break;
-        case "flying":
-            content.classList.add('babyblue');
-            break;
-        case "poison":
-            content.classList.add('purple');
-            break;
-        case "bug":
-            content.classList.add('brightgreen');
-            break;
-        case "ice":
-            content.classList.add('darkwhite');
-            break;
-        case "ground":
-            content.classList.add('brown');
-            break;
-        case "rock":
-            content.classList.add('grey');
-            break;
-        case "water":
-            content.classList.add('blue');
-            break;
-        case "electric":
-            content.classList.add('yellow');
-            break;
-        case "steel":
-            content.classList.add('grey');
-            break;
-        case "dragon":
-            content.classList.add('red');
-            break;
-        case "fairy":
-            content.classList.add('pink');
-            break;
-        default:
-            content.classList.add('normal');
     }
 }
 
@@ -199,7 +116,7 @@ function openPokemoncardHTML(name, pokemon, i) {
 }
 
 async function openCardBefore(i) {
-    let pokemons = {results: filteredPokemons};
+    let pokemons = await loadMainPokemonData(); ;
     i = i - 1;
     if (i < 0) {
         i = pokemons.results.length - 1;
@@ -208,7 +125,7 @@ async function openCardBefore(i) {
 }
 
 async function openCardAfter(i) {
-    let pokemons = {results: filteredPokemons};
+    let pokemons = await loadMainPokemonData(); ;
     i = i + 1;
     if (i > pokemons.results.length - 1) {
         i = 0;
@@ -217,15 +134,13 @@ async function openCardAfter(i) {
 }
 
 async function loadAbouts(i) {
-    // loadAboutsVa == true;
-    // loadBasestateVa == false;
     let content = document.getElementById('infoContent');
     content.innerHTML = '';
     let button = document.getElementById('about');
     let buttonBase = document.getElementById('basestate');
     button.classList.add('loadBorder');
     buttonBase.classList.remove('loadBorder');
-    let pokemon = await loadPokemon(filteredPokemons[i].name);
+    let pokemon = await loadPokemon(i+1);
     content.innerHTML = loadAboutsHTML(pokemon);
 }
 
@@ -239,8 +154,6 @@ function loadAboutsHTML(pokemon) {
 }
 
 async function loadBasestate(i) {
-    // loadBasestateVa == true;
-    // loadAboutsVa == false;
     let content = document.getElementById('infoContent');
     content.innerHTML = '';
     let button = document.getElementById('about');
@@ -248,7 +161,7 @@ async function loadBasestate(i) {
     button.classList.remove('loadBorder');
     buttonBase.classList.add('loadBorder');
 
-    let pokemon = await loadPokemon(filteredPokemons[i].name);
+    let pokemon = await loadPokemon(i+1);
     content.innerHTML = pokemonStats(pokemon);
 }
 
@@ -266,7 +179,6 @@ function closePokemonCard() {
 async function loadMainPokemonData() {
     let response = await fetch(startUrl);
     let responseToJson = await response.json();
-    allPokemons = responseToJson.results; // Hier die allPokemons Liste füllen
     return responseToJson;
 }
 
